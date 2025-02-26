d3ce3f6d861bf04a05e152fba4147fd4f6d3f172
Fabio Flauzino <fabio@mfrural.com.br>
2025-02-26 17:49:58 -0300

    ajuste texto whatsapp

--- src/components/Buttom/ButtonFlutuante.tsx
+++ src/components/Buttom/ButtonFlutuante.tsx
@@ -13,7 +13,7 @@ export function ButtonFlutuante({ children, className, ...rest }: Props) {
       {...rest}
       onClick={() =>
         window.open(
-          "https://wa.me/5514998003555?text=Gostaria%20de%20agendar%20um%20hora%CC%81rio%20ou%20consulta%20com%C2%A0a%C2%A0Dra.%C2%A0Thai%CC%81s",
+          "https://wa.me/5514998003555?text=Gostaria%20de%20agendar%20um%20hora%CC%81rio%20com%C2%A0a%C2%A0Dra.%C2%A0Thai%CC%81s",
           "_blank"
         )
       }
